8b2aad8c75e23e63076450f545933118fea5a479
Foxsage52 <145737393+Foxsage52@users.noreply.github.com>
2024-09-08 18:06:25 -0400

    Update dictionaries.py

--- Notes and Items/Misc Notes/dictionaries.py
+++ Notes and Items/Misc Notes/dictionaries.py
@@ -1,2 +1,12 @@
 # Dictionaries
+band = {
+    "Vocals": "Plant",
+    "Guitar": "Page"
+}
 
+band2 = dict(vocals="Plant", guitar="Page")
+
+print(band)
+print(band2)
+print(type(band))
+print(len(band))
