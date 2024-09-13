e78aaa36810dca639614483862413329cf30b5e9
Foxsage52 <145737393+Foxsage52@users.noreply.github.com>
2024-09-12 21:10:58 -0400

    Create scope.py

---
+++ Notes and Items/Misc Notes/scope.py
@@ -0,0 +1,22 @@
+
+
+name = "Juan"
+count = 1
+
+
+def another():
+    color = "blue"
+    global count
+    count += 1
+    print(count)
+
+    def greeting(name):
+        nonlocal color
+        color = "red"
+        print(color)
+        print(name)
+
+    greeting("Juan")
+
+
+another()
